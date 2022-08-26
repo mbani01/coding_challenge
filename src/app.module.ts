9a7218ba26b41b1a8432da041bd7342a133dc5d9
Mbani <mouad-mb@outlook.com>
2022-08-26 11:51:23 +0100

    create Observation resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { ObservationModule } from './observation/observation.module';
 import * as  typeOrmConfig  from './typeorm.config'
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
+    ObservationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
