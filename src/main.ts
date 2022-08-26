4fac4715aa4b52891ae7c6800b11ae8eb326b8eb
Mbani <mouad-mb@outlook.com>
2022-08-26 10:07:22 +0100

    setting Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
+
+  const config = new DocumentBuilder()
+    .setTitle('Observations API')
+    .setDescription('Simple CRUD for observations')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
